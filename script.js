89db5cb61468047951b899df1eaa1270e1b0a099
donha <miguel.moreira.silva22@escola.pr.gov.br>
2025-06-24 10:17:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const avanca = document.querySelectorAll('.btn-proximo');
+document.addEventListener('DOMContentLoaded', () => {
+    const botoesProximo = document.querySelectorAll('.btn-proximo');
+    const passos = document.querySelectorAll('.passo');
 
-avanca.forEach(button => {
-    button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+    botoesProximo.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const proximoPassoId = event.target.dataset.proximo;
+            mostrarPasso(proximoPassoId);
+        });
+    });
 
-        atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
-    })
-})
+    function mostrarPasso(id) {
+        passos.forEach(passo => {
+            passo.classList.remove('ativo'); // Remove a classe 'ativo' de todos os passos
+        });
+
+        const passoAtivo = document.getElementById(`passo-${id}`);
+        if (passoAtivo) {
+            passoAtivo.classList.add('ativo'); // Adiciona a classe 'ativo' ao passo selecionado
+        }
+    }
+});
